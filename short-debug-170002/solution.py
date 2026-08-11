e61a3b575e83c1633e6629bbea93ef0848184bd7
OMM <timepasshub2539@gmail.com>
2026-08-11 17:08:11 +0530

    Add Debug Short

---
+++ short-debug-170002/solution.py
@@ -0,0 +1,6 @@
+Traceback (most recent call last):
+  File "api.py", line 42, in handle
+    result = average(items)
+  File "stats.py", line 2, in average
+    return sum(nums) / len(nums)
+ZeroDivisionError: division by zero
